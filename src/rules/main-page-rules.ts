9f89012828faf7f0074fdfc829af6b79ec677933
lxp <hello_xp@outlook.com>
2022-07-21 22:06:45 +0800

    更新一些规则

--- src/rules/main-page-rules.ts
+++ src/rules/main-page-rules.ts
@@ -1,5 +1,5 @@
 import { checkMatchLike } from "@/settings/matches"
-import { checkUsernameLike, checkUsername } from "@/settings/uids-usernames"
+import { checkUsername, checkUsernameLike } from "@/settings/uids-usernames"
 import { registerRule } from "./base-rules"
 /**
  * 首页规则
@@ -26,6 +26,11 @@ export function initMainPage() {
         node => checkMatchLike(node.getAttribute('alt'))
     )
     registerRule(
+        '.van-slide .item',  // 左上角滑动图片
+        'i.bypb-icon',  // 广告
+        () => true
+    )
+    registerRule(
         '.video-card-common',  // 左边的视频块
         'a.title',  // 标题(matches)
         node => checkMatchLike(node.getAttribute('title'))
@@ -34,5 +39,15 @@ export function initMainPage() {
         '.video-card-common',  // 左边的视频块
         'a.up',  // UP(username)  // 有其他内容, 不是单一用户名
         node => checkUsernameLike(node.innerHTML)
+    )
+    registerRule(
+        '.video-card-common',  // 左边的视频块
+        '.gg-normal-icon',  // 广告
+        () => true
+    )
+    registerRule(
+        'a.banner-card.b-wrap',  // 横条广告
+        '.gg-icon',
+        () => true
     )
 }
