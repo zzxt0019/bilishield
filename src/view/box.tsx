184a44612b213c2f8047a8647f54d4e93f770824
lxp <hello_xp@aliyun.com>
2022-08-17 16:30:12 +0800

    box

--- src/view/box.tsx
+++ src/view/box.tsx
@@ -1,97 +1,91 @@
 import {Settings} from "@/config/setting/setting";
 import {Button, Card, Col, Layout, Row, Tabs} from "antd";
-import React from "react";
+import React, {useEffect, useRef} from "react";
 import {Page} from "@/config/page/page";
 import {PageView} from "./page.view";
 import {SettingView} from "./setting.view";
 import {UidUsernameView} from "./special/uid-username.view";
 import {DisplayType} from "@/view/display-type";
 
-export class Box extends React.Component {
-    REFS: any = {
-        // 绑定ref
-        main: React.createRef<HTMLDivElement>()
-    }
-    props = {
-        mainClass: '_main',
-        boxClass: '_box',
-        pageMap: new Map<string, Page>()
-    }
-
-    componentWillUnmount(): void {
-        for (const page of this.props.pageMap.values()) {
-            if (page.working) {
-                page.stop()
+export function Box(props: {
+    mainClass: string,
+    boxClass: string,
+    pageMap: Map<string, Page>
+}) {
+    const {mainClass, boxClass, pageMap} = props
+    const mainRef = useRef<HTMLDivElement>()
+    useEffect(() => {
+        return () => {
+            for (const page of props.pageMap.values()) {
+                if (page.working) {
+                    page.stop()
+                }
             }
         }
-    }
-
-    render() {
-        return <div className={this.props.mainClass} ref={this.REFS.main} style={{display: 'none'}}>
-            <div className={this.props.boxClass}>
-                {(() => {
-                    if ([...this.props.pageMap.values()].filter(page => page.isCurrent()).length !== 0)
-                        return <Layout>
-                            <Layout.Content>
-                                {  // 页面
-                                    [...this.props.pageMap.values()].filter(page => page.isCurrent()).map(page =>
-                                        <PageView page={page} updateBox={() => {
-                                            // 更新当前页面, PageView里面有page对象, 所以可以放在里面(也可以放在外面)
-                                            this.forceUpdate()
-                                        }}/>)}
-                            </Layout.Content>
-                            <Layout.Sider theme="light">
-                                <DisplayType></DisplayType>
-                            </Layout.Sider>
-                        </Layout>
-                })()}
-                <hr/>
-                <Tabs>
-                    <Tabs.TabPane key="uid2username-tab" tab={<span>uid</span>}>
-                        <UidUsernameView updateBox={() => {
-                            // 需要更改所有内容, 需要放在外面, SettingView里不需要page信息
-                            for (const page of this.props.pageMap.values()) {
-                                if (page.working) {
-                                    page.stop()
-                                    page.start()
-                                }
-                            }
-                            this.forceUpdate()
-                        }}/>
-                    </Tabs.TabPane>
-                    {  // 配置
-                        [...Settings.getSystemSettings().values()].map(setting =>
-                            <Tabs.TabPane key={setting.key + '-tab'} tab={<span>{setting.name}</span>}>
-                                <SettingView
-                                    key={setting.key}
-                                    setting={setting}
-                                    updateBox={() => {
-                                        // 需要更改所有内容, 需要放在外面, SettingView里不需要page信息
-                                        for (const page of this.props.pageMap.values()) {
-                                            if (page.working) {
-                                                page.stop()
-                                                page.start()
-                                            }
-                                        }
+    })
+    return <div className={mainClass} ref={mainRef} style={{display: 'none'}}>
+        <div className={boxClass}>
+            {(() => {
+                if ([...pageMap.values()].filter(page => page.isCurrent()).length !== 0)
+                    return <Layout>
+                        <Layout.Content>
+                            {  // 页面
+                                [...pageMap.values()].filter(page => page.isCurrent()).map(page =>
+                                    <PageView page={page} updateBox={() => {
+                                        // 更新当前页面, PageView里面有page对象, 所以可以放在里面(也可以放在外面)
                                         this.forceUpdate()
-                                    }}/>
-                            </Tabs.TabPane>)}
-                </Tabs>
-                <hr/>
-                <Card>
-                    <Row>
-                        <Col span={8}></Col>
-                        <Col span={8}></Col>
-                        <Col span={8}>
-                            <Button size="small"
-                                    onClick={() => {
-                                        this.REFS.main.current.style.setProperty('display', 'none')
-                                    }}>隐藏菜单</Button>
-                        </Col>
-                    </Row>
-                </Card>
-            </div>
-
+                                    }}/>)}
+                        </Layout.Content>
+                        <Layout.Sider theme="light">
+                            <DisplayType></DisplayType>
+                        </Layout.Sider>
+                    </Layout>
+            })()}
+            <hr/>
+            <Tabs>
+                <Tabs.TabPane key="uid2username-tab" tab={<span>uid</span>}>
+                    <UidUsernameView updateBox={() => {
+                        // 需要更改所有内容, 需要放在外面, SettingView里不需要page信息
+                        for (const page of pageMap.values()) {
+                            if (page.working) {
+                                page.stop()
+                                page.start()
+                            }
+                        }
+                        this.forceUpdate()
+                    }}/>
+                </Tabs.TabPane>
+                {  // 配置
+                    [...Settings.getSystemSettings().values()].map(setting =>
+                        <Tabs.TabPane key={setting.key + '-tab'} tab={<span>{setting.name}</span>}>
+                            <SettingView
+                                key={setting.key}
+                                setting={setting}
+                                updateBox={() => {
+                                    // 需要更改所有内容, 需要放在外面, SettingView里不需要page信息
+                                    for (const page of pageMap.values()) {
+                                        if (page.working) {
+                                            page.stop()
+                                            page.start()
+                                        }
+                                    }
+                                    this.forceUpdate()
+                                }}/>
+                        </Tabs.TabPane>)}
+            </Tabs>
+            <hr/>
+            <Card>
+                <Row>
+                    <Col span={8}></Col>
+                    <Col span={8}></Col>
+                    <Col span={8}>
+                        <Button size="small"
+                                onClick={() => {
+                                    mainRef.current.style.setProperty('display', 'none')
+                                }}>隐藏菜单</Button>
+                    </Col>
+                </Row>
+            </Card>
         </div>
-    }
+    </div>
 }
