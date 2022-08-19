9bd704b6d0af93b19a728e93cfe6798aae60ada0
lxp <hello_xp@aliyun.com>
2022-08-19 09:45:09 +0800

    start判断是否存在key 可反复调用start

--- src/observer/impl/my-observer.ts
+++ src/observer/impl/my-observer.ts
@@ -6,18 +6,20 @@ export class MyObserver extends Observer {
     observerMap: Map<string, MutationObserver> = new Map()
 
     start(rule: DoRuleN, window: Window, windowKey: number) {
-        let mains = window.document.querySelectorAll(rule.mainSelector + ':not(.' + DISPLAY_CLASS + ')');
-        for (let i = 0; i < mains.length; i++) {
-            rule.display(mains[i])
-        }
-        let observer = new MutationObserver(() => {
+        if (!this.observerMap.has(windowKey + ':' + rule.mainSelector)) {
             let mains = window.document.querySelectorAll(rule.mainSelector + ':not(.' + DISPLAY_CLASS + ')');
             for (let i = 0; i < mains.length; i++) {
                 rule.display(mains[i])
             }
-        });
-        observer.observe(window.document, {childList: true, subtree: true})
-        this.observerMap.set(windowKey + ':' + rule.mainSelector, observer);
+            let observer = new MutationObserver(() => {
+                let mains = window.document.querySelectorAll(rule.mainSelector + ':not(.' + DISPLAY_CLASS + ')');
+                for (let i = 0; i < mains.length; i++) {
+                    rule.display(mains[i])
+                }
+            });
+            observer.observe(window.document, {childList: true, subtree: true})
+            this.observerMap.set(windowKey + ':' + rule.mainSelector, observer);
+        }
     }
 
     stop(rule: DoRuleN, window: Window, windowKey: number) {
