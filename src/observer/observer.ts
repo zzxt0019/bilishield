bdf6a1725093a5ef07540bb0aaccd7c9964c7d0e
lxp <hello_xp@aliyun.com>
2022-08-19 10:00:10 +0800

    优化my-observer.ts结构

--- src/observer/observer.ts
+++ src/observer/observer.ts
@@ -1,6 +1,21 @@
 import {DoRuleN} from "@/config/rule/do-rule";
 
 export abstract class Observer {
+    protected windowKey(window0: Window): number {
+        if (window0 === window) {
+            return -1;
+        }
+        for (let i = 0; i < window.frames.length; i++) {
+            if (window0 === window.frames[i]) {
+                return i;
+            }
+        }
+        return -1;  // 都没有是错的, 默认-1为主window(然后不做处理)
+    }
+
+    protected wrKey(rule: DoRuleN, window0: Window): string {
+        return this.windowKey(window0) + ':' + rule.mainSelector;
+    }
 
     abstract start(rule: DoRuleN, window: Window, windowKey: number): void
 
