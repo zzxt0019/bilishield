251f6ef9dcf6cd2841b2d9d54012e5644155e8dd
lxp <zzxt0019@outlook.com>
2023-01-06 17:07:36 +0800

    workflows

--- script/node.js
+++ script/node.js
@@ -2,31 +2,37 @@ const fs = require('fs');
 const path = require('path');
 const highlight = require('highlight.js').default;
 const marked = require('marked');
-const _html = 'html';
+const _html = 'iframes';
 const _css = 'github.css';
 const ignores = [_html, 'data.json', 'index.js', 'index.html'];
 
+/**
+ * 递归读取文件
+ */
 function readFile(filePath, json, fileCallback) {
+    // 不读取的部分
     if (ignores.includes(filePath)) {
         return;
     }
     if (fs.lstatSync(path.resolve(__dirname, '../build') + `/${filePath}`).isDirectory()) {  // 是文件夹
+        // 创建html文件夹内的对应的文件夹
         if (!fs.existsSync(path.resolve(__dirname, `../build/${_html}`) + `/${filePath}`)) {
             fs.mkdirSync(path.resolve(__dirname, `../build/${_html}`) + `/${filePath}`);
         }
+        // 读取文件
         fs.readdirSync(path.resolve(__dirname, '../build') + `/${filePath}`)
             .forEach(file => {
                 readFile((filePath ? filePath + '/' : '') + file, json, fileCallback)
             });
-    } else if (fs.lstatSync(path.resolve(__dirname, '../build') + `/${filePath}`).isFile()) {
+    } else if (fs.lstatSync(path.resolve(__dirname, '../build') + `/${filePath}`).isFile()) {  // 是文件
+        // 处理
         fileCallback(filePath, json);
     }
 }
 
-/**
- * 生成文件json
- */
-function json() {
+
+function afterBuild() {
+    // 预先创建html文件夹
     if (!fs.existsSync(path.resolve(__dirname, `../build/${_html}`))) {
         fs.mkdirSync(path.resolve(__dirname, `../build/${_html}`));
     }
@@ -47,13 +53,16 @@ function json() {
         // html
         fs.writeFileSync(path.resolve(__dirname, `../build/${_html}`) + `/${filePath}.html`, createMarkdownHtml(fs.readFileSync(path.resolve(__dirname, '../build') + `/${filePath}`), filePath));
     });
+    // 写data.json
     fs.writeFileSync(path.resolve(__dirname, '../build') + '/data.json', JSON.stringify({
         data: json, timestamp: Date.now(),
     }));
+    // 复制highlight的css
     fs.copyFileSync(path.resolve(__dirname, '../node_modules') + `/highlight.js/styles/${_css}`, path.resolve(__dirname, `../build/${_html}/${_css}`));
 }
 
 function createMarkdownHtml(text, filePath) {
+    // 创建空文件 计算css相对位置
     fs.writeFileSync(path.resolve(__dirname, `../build/${_html}`) + `/${filePath}.html`, '');
     marked.setOptions({
         highlight: function (code) {
@@ -73,9 +82,7 @@ function createMarkdownHtml(text, filePath) {
 <head>
     <meta charset="UTF-8">
     <title>bilishield</title>
-    <link href="${path.relative(
-        path.resolve(__dirname, './build') + `/${_html}/${filePath}.html`,
-        path.resolve(__dirname, './build') +`/${_html}/${_css}` )}"  type="text/css" rel="stylesheet">
+    <link href="${path.relative(path.dirname(path.resolve(__dirname, './build') + `/${_html}/${filePath}.html`), path.resolve(__dirname, './build') + `/${_html}/${_css}`)}"  type="text/css" rel="stylesheet">
 </head>
 <body>
 ${body}
@@ -83,4 +90,4 @@ ${body}
 </html>`
 }
 
-json();
+afterBuild();
