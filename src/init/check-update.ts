81b64be6c62056a39bb841db81b0809c2b7a0790
lxp <zzxt0019@outlook.com>
2023-01-07 16:09:57 +0800

    todo check-update

--- src/init/check-update.ts
+++ src/init/check-update.ts
@@ -3,19 +3,35 @@ import * as yaml from "yaml";
 export async function checkVersion() {
     let version: string = GM_getValue('script.version') ?? '1.0';
     let configs = ['page', 'rule', 'setting'];
-    if (GM_info.script.version > version) {  // 存储版本 < 当前版本 => 更新配置
-        let promises: Promise<Response>[] = [];
+    // if (GM_info.script.version > version) {  // 存储版本 < 当前版本 => 更新配置
+        let jsonPromises: Promise<Response>[] = [];
         configs.forEach(key => {
-            promises.push(fetch(GM_info.script.icon64?.substring(0, 0x2A) + '/yaml/' + key + '.yaml'));
+            jsonPromises.push(fetch(GM_info.script.icon64?.substring(0, 0x25) + '/yaml/' + key + '/data.json'));
         })
-        let responses = await Promise.all(promises);
-        for (let i = 0; i < configs.length; i++) {
-            GM_setValue('script.' + configs[i], yaml.parse(await responses[i].text()));
-        }
-        GM_setValue('script.version', GM_info.script.version);
-    } else if (GM_info.script.version < version) {  // 存储版本 > 当前版本(本地测试版本为0.0) => 是本地测试, 读取本地yaml
-        configs.forEach(config => {
-            GM_setValue('script.' + config, yaml.parse(GM_getResourceText(config)));
+        let jsonResponses = await Promise.all(jsonPromises);
+    let yamlPromises: Promise<Response>[][] = [];
+    for (let i = 0; i < configs.length; i++) {
+        let arr = await jsonResponses[i].json();
+        yamlPromises.push([])
+        arr.forEach((yaml:any)=>{
+            yamlPromises[i].push(fetch(GM_info.script.icon64?.substring(0, 0x25) + '/yaml/' + configs[i] + yaml));
         })
     }
+    for (let i = 0; i < yamlPromises.length; i++) {
+        let yamlResponses = await Promise.all(yamlPromises[i]);
+        yamlResponses.forEach(async yamlResponse=>{
+            console.log(configs[i] , yaml.parse(await yamlResponse.text()));
+        })
+    }
+
+
+        // for (let i = 0; i < configs.length; i++) {
+        //     GM_setValue('script.' + configs[i], yaml.parse(await responses[i].text()));
+        // }
+    //     GM_setValue('script.version', GM_info.script.version);
+    // } else if (GM_info.script.version < version) {  // 存储版本 > 当前版本(本地测试版本为0.0) => 是本地测试, 读取本地yaml
+    //     configs.forEach(config => {
+    //         GM_setValue('script.' + config, yaml.parse(GM_getResourceText(config)));
+    //     })
+    // }
 }
